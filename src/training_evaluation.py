fixed scaling on ortho loss

src/training_evaluation.py
@@ -45,7 +45,6 @@ def OrthogonalityLoss(eigvecs):
     """
     # 1) Gram matrix G [k × k]
     G = eigvecs.transpose(-2, -1) @ eigvecs    # shape (..., k, k)
-
     # 2) Identity of size k on the same device & dtype
     k = G.size(-1)
     I = torch.eye(k, device=G.device, dtype=G.dtype)
@@ -85,9 +84,12 @@ def train(model, loader, optimizer, device, config):
         optimizer.step()
         
 
-        total_loss += loss.item() * data.x.size(0)
-        total_energy_loss += energy_loss.item() * data.x.size(0) 
-        total_ortho_loss += ortho_loss.item() * data.x.size(0) 
+        batch_size = int(data.batch.max()) + 1
+        total_loss += loss.item()
+        total_energy_loss += energy_loss.item()
+        total_ortho_loss += ortho_loss.item()
+
+
 
     total_loss = total_loss / len(loader.dataset)
     total_energy_loss = total_energy_loss / len(loader.dataset)
@@ -110,9 +112,10 @@ def validate(model, loader, optimizer, device, config):
         ortho_loss = OrthogonalityLoss(out)
         loss = config.lambda_energy * energy_loss + config.lambda_ortho * ortho_loss
 
-        total_loss += loss.item() * data.x.size(0)
-        total_energy_loss += energy_loss.item() * data.x.size(0) 
-        total_ortho_loss += ortho_loss.item() * data.x.size(0) 
+        batch_size = int(data.batch.max()) + 1
+        total_loss += loss.item()
+        total_energy_loss += energy_loss.item() 
+        total_ortho_loss += ortho_loss.item() 
 
     total_loss = total_loss / len(loader.dataset)
     total_energy_loss = total_energy_loss / len(loader.dataset)

src/utils.py
@@ -90,13 +90,13 @@ def convert_scipy_to_torch_sparse(matrix):
     return matrix
 
 
-def collate_fn(batch):
-    # batch is a list of tuple (graph, label)
-    graphs = [e[0] for e in batch]
-    g = dgl.batch(graphs)
-    labels = [e[1] for e in batch]
-    labels = torch.stack(labels, 0)
-    return g, labels
+# def collate_fn(batch):
+#     # batch is a list of tuple (graph, label)
+#     graphs = [e[0] for e in batch]
+#     g = dgl.batch(graphs)
+#     labels = [e[1] for e in batch]
+#     labels = torch.stack(labels, 0)
+#     return g, labels
 
 
 def load_data(config):
@@ -107,9 +107,9 @@ def load_data(config):
     split_idx = dataset.get_idx_split()
 
     
-    train_loader = DataLoader(dataset[split_idx['train']], batch_size=32, shuffle=True, collate_fn=collate_fn) # ISSUE: right now this is just concatenating everything in the batch, treating it lke a huge graph
-    val_loader   = DataLoader(dataset[split_idx['valid']], batch_size=64, shuffle=False, collate_fn=collate_fn)
-    test_loader  = DataLoader(dataset[split_idx['test']],  batch_size=64, shuffle=False, collate_fn=collate_fn)
+    train_loader = DataLoader(dataset[split_idx['train']], batch_size=32, shuffle=True) # ISSUE: right now this is just concatenating everything in the batch, treating it lke a huge graph
+    val_loader   = DataLoader(dataset[split_idx['valid']], batch_size=64, shuffle=False)
+    test_loader  = DataLoader(dataset[split_idx['test']],  batch_size=64, shuffle=False)
 
 
     return dataset, train_loader, val_loader, test_loader